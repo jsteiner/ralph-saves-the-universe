0cbe81a86b8f7638f51a0786112cbe09c34bd9f0
Josh Steiner <josh@jsteiner.me>
2014-07-24 14:16:25 -0400

    Compile stylesheets

--- Brocfile.js
+++ Brocfile.js
@@ -1,6 +1,7 @@
 var filterCoffeeScript = require('broccoli-coffee');
 var pickFiles = require('broccoli-static-compiler');
 var compileES6 = require('broccoli-es6-concatenator');
+var compileSass = require('broccoli-sass');
 var mergeTrees = require('broccoli-merge-trees');
 var findBowerTrees = require('broccoli-bower');
 
@@ -19,9 +20,18 @@ javascript = pickFiles(javascript, {
 })
 javascript = preprocess(javascript);
 
+var stylesheets = 'stylesheets'
+stylesheets = pickFiles(stylesheets, {
+  srcDir: '',
+  destDir: 'appkit'
+});
+stylesheets = preprocess(stylesheets);
+
 var vendor = 'vendor';
 
-var sourceTrees = [javascript, vendor].concat(findBowerTrees());
+var sourceTrees = [javascript, stylesheets, vendor]
+sourceTrees = sourceTrees.concat(findBowerTrees());
+
 var appAndDependencies = new mergeTrees(sourceTrees, { overwrite: true });
 
 var appJs = compileES6(appAndDependencies, {
@@ -33,8 +43,10 @@ var appJs = compileES6(appAndDependencies, {
     'phaser.js'
   ],
   outputFile: '/assets/app.js'
-})
+});
+
+var appCss = compileSass(sourceTrees, 'appkit/app.scss', 'assets/app.css');
 
 var publicFiles = 'public';
 
-module.exports = mergeTrees([appJs, publicFiles]);
+module.exports = mergeTrees([appJs, appCss, publicFiles]);
